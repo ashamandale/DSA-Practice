5f2aed80e4fa97ff3b79d191e09e523bfd4b3681
Asha Mandale <87373443+ashamandale@users.noreply.github.com>
2025-12-05 21:27:26 +0530

    code updated

--- Array/Assignment/Q1.cpp
+++ Array/Assignment/Q1.cpp
@@ -2,6 +2,7 @@
 //least twice in the array,and return false if every element is distinct.
 
 #include<iostream>
+#include<algorithm>
 using namespace std;
 int containDuplicate(int *nums,int n)
 {
@@ -16,21 +17,22 @@ int containDuplicate(int *nums,int n)
     return flag;
 }
 
-int containDuplicate2(int *nums,int n)
+int containDuplicate2(int *nums,int n)  
 {
     bool flag=false; //No Duplicates
+    sort(nums, nums + n);
     for(int i=0;i<n-1;i++){
          if(nums[i]==nums[i+1]){
             flag=true; //Duplicate found
          }         
     }
     
-    return flag;
+   return flag;
 }
 int main(){
-    int arr[]={1,2,3,4};
+    int arr[]={1,2,3,4,1};
     int n=sizeof(arr)/sizeof(int);
-    cout<<containDuplicate(arr,n)<<endl;
-    //cout<<containDuplicate2(arr,n);
+    //cout<<containDuplicate(arr,n)<<endl;
+    cout<<containDuplicate2(arr,n);
     return 0;
 }
