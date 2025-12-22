17e4e8f285640b08ec3cd1a16f4a4bf5230e5ddf
Asha Mandale <87373443+ashamandale@users.noreply.github.com>
2025-12-22 22:30:04 +0530

    code updated

--- Array/2D Array/diagonalSum.cpp
+++ Array/2D Array/diagonalSum.cpp
@@ -3,29 +3,29 @@ using namespace std;
 int digonalSum(int mat[][4],int n)//n*n size matrix
 {
 int sum=0;
-// First Approch O(n^2)
-// for (int i=0;i<n;i++){
-//     for(int j=0;j<n;j++)
-//     {
-//         if(i==j){
-//           sum+=mat[i][j];
-//         }
-//         else if(j==n-i-1)
-//         {
-//             sum+=mat[i][j];
-//         }
-//     }
-// }
-
-//Second Approch O(n)
-for(int i=0;i<n;i++){ //Primary diagonal
-    sum+=mat[i][i];
-    if (i!=n-i-1)   //Secondary diagonal
+//First Approch O(n^2)
+for (int i=0;i<n;i++){
+    for(int j=0;j<n;j++)
     {
-        sum+=mat[i][n-i-1];
+        if(i==j){
+          sum+=mat[i][j];
+        }
+        else if(j==n-i-1)
+        {
+            sum+=mat[i][j];
+        }
     }
-    
 }
+
+// //Second Approch O(n)
+// for(int i=0;i<n;i++){ //Primary diagonal
+//     sum+=mat[i][i];
+//     if (i!=n-i-1)   //Secondary diagonal
+//     {
+//         sum+=mat[i][n-i-1];
+//     }
+    
+// }
 cout<<"Diagonal Sum:"<<sum;
 return sum;
 
