57950ca62044e35582c8ba8730bd930e325d76db
Asha Mandale <87373443+ashamandale@users.noreply.github.com>
2025-12-31 14:10:09 +0530

    code updated

---
+++ Strings/validAnagram.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+bool isAnagram(string str1, string str2){
+    if(str1.length() != str2.length()){
+        cout<<"Not a valid Anagram";
+        return false;
+    }
+    int count[26]={0};
+    for(int i=0;i<str1.length();i++){
+        int idx=str1[i]-'a';
+        count[idx]++;
+    }
+    for(int i=0;i<str2.length();i++){
+        int idx=str2[i]-'a';
+        if(count[idx]== 0){
+            cout<<"Not a valid Anagram";
+            return false;
+        }
+        count[idx]--;
+    }
+cout<<"valid Anagram";
+return true;
+}
+
+int main(){
+    string str1="listen";
+    string str2="silent";
+    isAnagram(str1,str2);
+    return 0;
+}
