68c45952c7d91eca6d90ae80a06a4864346ba7ed
Asha Mandale <87373443+ashamandale@users.noreply.github.com>
2026-01-04 22:41:12 +0530

    code updated

--- Vectors/Assignment/Q1.cpp
+++ Vectors/Assignment/Q1.cpp
@@ -9,7 +9,7 @@ vector<int> findErrorNums(vector<int>& nums){
         freq[nums[i]]++;
     }
     int duplicate=-1,missing=-1;
-    for(int i=1;i<n;i++){
+    for(int i=1;i<=n;i++){
         if(freq[i]==2){
             duplicate=i;
         }else if(freq[i]==0){
