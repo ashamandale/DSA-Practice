7865d90d236493175318d246bbab46e9c87ffc9f
Asha Mandale <87373443+ashamandale@users.noreply.github.com>
2026-02-04 12:44:24 +0530

    code updated

---
+++ Recursion/binSearch.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int binarySearch(int arr[],int start,int end,int key){
+    if(start > end ){
+        return -1;
+    }
+    int mid=(start+end)/2;
+    
+    //key found
+    if(arr[mid] ==key){
+        return mid;
+    }
+    //Search in left half
+    if(key < arr[mid]){
+        return binarySearch (arr,start,mid-1,key);
+    }
+
+    //Search in right half
+    if(key > arr[mid]){
+        return binarySearch (arr,mid+1,end,key);
+    }
+}
+int main(){
+    int arr[]={1,2,3,4,5,6,7};
+    int n=7;
+    int key=2;
+    int index=binarySearch(arr,0,n-1,key);
+    cout<<index;
+    return 0;
+}
