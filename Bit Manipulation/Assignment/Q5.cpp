970c64fc8d0b35cdc7cde327da3b8ac1ec8139a0
Asha Mandale <87373443+ashamandale@users.noreply.github.com>
2026-02-21 22:37:47 +0530

    code updated

---
+++ Bit Manipulation/Assignment/Q5.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int countPrimeSetBits(int left, int right) {
+        unordered_set<int> primes = {2,3,5,7,11,13,17,19};
+        
+        int count = 0;
+        
+        for(int i = left; i <= right; i++) {
+            int bits = __builtin_popcount(i);
+            if(primes.count(bits))
+                count++;
+        }
+        
+        return count;
+    }
+};
+
+int main() {
+    int left, right;
+    cout << "Enter left and right: ";
+    cin >> left >> right;
+
+    Solution obj;
+    int result = obj.countPrimeSetBits(left, right);
+
+    cout << "Count of numbers with prime set bits: " << result << endl;
+
+    return 0;
+}
