8b08671e3d940686d2fa4e7166d3799d6aae3fbb
Asha Mandale <87373443+ashamandale@users.noreply.github.com>
2026-03-10 15:36:23 +0530

    code updated

---
+++ Backtracking/gridWays.cpp
@@ -0,0 +1,31 @@
+//Gridways
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+int gridWays(int r,int c,int n,int m,string ans){
+    if(r == n-1 && c== m-1){  //base case
+        cout<<ans<<endl;
+        return 1 ;
+    }
+
+    if(r>=n || c>=m){
+        return 0;
+    }
+    //right
+    int val1=gridWays(r,c+1,n,m,ans+"R");
+
+    //Down
+    int val2=gridWays(r+1,c,n,m,ans+"D");
+
+
+  return val1+val2;
+}
+int main(){
+    int n=3;
+    int m=3; //3x3
+    string ans="";
+    cout<<gridWays(0,0,n,m,ans);
+    
+    return 0;
+}
