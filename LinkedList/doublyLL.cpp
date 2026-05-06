023fe1fe7a1bacd1f178f536f3bfadea88770762
Asha Mandale <87373443+ashamandale@users.noreply.github.com>
2026-05-06 15:00:43 +0530

    code updated

--- LinkedList/doublyLL.cpp
+++ LinkedList/doublyLL.cpp
@@ -16,7 +16,7 @@ class doublyLL{
     public:
     Node* head;
     Node* tail;
-    
+
  doublyLL(){
     head=tail=NULL;
  }   
@@ -31,6 +31,45 @@ class doublyLL{
         head->prev=newnode;
         head = newnode;
     }
+ }
+
+ void pop_front(){
+    Node* temp =head;
+    head=head->next;
+
+    if(head!=NULL){
+        head->prev=NULL;
+    }
+    temp->next=NULL;
+    delete temp;
+ }
+
+ void push_back(int val){
+    Node* newNode= new Node(val);
+    if(head==NULL){
+        head=tail=newNode;
+    }else{
+        newNode->prev=tail;
+        tail->next=newNode;
+        tail=newNode;
+    }
+ }
+
+ void pop_back(){
+    //Empty list
+    if(head==NULL){
+        return;
+    }
+    //Single node
+    if(head==tail){
+        delete tail;
+        head=tail=NULL;
+        return;
+    }
+    Node* temp=tail;
+    tail=tail->prev;
+    tail->next=NULL;
+    delete temp;
  }
 
  void printList(){
@@ -49,6 +88,12 @@ int main(){
     dbll.push_front(3);
     dbll.push_front(2);
     dbll.push_front(1);
+    dbll.printList();
+    dbll.pop_front();
+    dbll.printList();
+    dbll.push_back(5);
+    dbll.printList();
+    dbll.pop_back();
     dbll.printList();
     
 
