f25074531edfff82a93979197b4655d5b5fde9fd
Asha Mandale <87373443+ashamandale@users.noreply.github.com>
2026-05-20 17:20:24 +0530

    code updated

---
+++ Stack/reverseStack.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+void pushAtBottom(stack<int> &s, int val){
+
+    if(s.empty()){
+        s.push(val);  //Push at top = push at bottom in empty stack
+        return;
+    }
+        int  temp= s.top();
+        s.pop();
+        pushAtBottom(s,val);
+        s.push(temp);
+    }
+
+    void reverse(stack<int> &s){
+        if(s.empty()){
+            return;        }
+        int temp=s.top();
+        s.pop();
+        reverse(s);
+        pushAtBottom(s,temp);
+
+    }
+
+    void printStack(stack<int> s){
+        while(!s.empty()){
+        cout<<s.top()<<" ";
+        s.pop();
+    }
+    cout<<"\n";
+    }
+int main(){
+    stack<int> s;
+    s.push(3);
+    s.push(2);
+    s.push(1);
+    printStack(s);
+    reverse(s);
+    printStack(s);
+    return 0;
+
+}
