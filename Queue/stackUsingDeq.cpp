6cd8336c07538ab6e1495b716a4eb7c63857e5ec
Asha Mandale <87373443+ashamandale@users.noreply.github.com>
2026-05-28 11:30:36 +0530

    code updated

---
+++ Queue/stackUsingDeq.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<queue>
+#include<deque>
+using namespace std;
+class Stack{
+ deque<int> deq;
+ public:
+ void push(int data){
+    deq.push_front(data);
+ }
+
+ void pop(){
+    deq.pop_front();
+ }
+
+ int  top(){
+    return deq.front();
+ }
+
+ bool empty(){
+   return deq.empty();
+ }
+};
+int main(){
+    Stack s;
+
+    for(int i=1;i<=5;i++){
+        s.push(i);
+    }
+
+    for(int i=1;i<=5;i++){
+        cout<<s.top()<<endl;
+        s.pop();
+    }
+return 0;
+}
