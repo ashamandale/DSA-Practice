18286541866288d391a539c13b6868b869ab6579
Asha Mandale <87373443+ashamandale@users.noreply.github.com>
2026-06-17 15:57:08 +0530

    code updated

--- Binary Tree/LCA.cpp
+++ Binary Tree/LCA.cpp
@@ -60,11 +60,30 @@ static int idx = -1;
         return lca;
     }
 
+    //Second Approch
+
+    Node* LCA2(Node* root,int n1,int n2){
+        if(root ==NULL){
+            return NULL;
+        }
+        if(root->data == n1 || root->data == n2){
+            return root;
+        }
+        Node* leftLCA= LCA2(root->left,n1,n2);
+        Node* rightLCA= LCA2(root->right,n1,n2);
+
+        if(leftLCA != NULL &&  rightLCA != NULL){
+            return root;
+        }
+      return leftLCA == NULL ? rightLCA:leftLCA;
+    }
+
 int main(){
     vector<int> nodes={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
     Node* root=buildTree(nodes);
     
-    int n1=4, n2=5;
-    cout<<" LCA :"<<LCA(root,n1,n2)<<endl;
+     int n1=4, n2=5;
+    // cout<<" LCA :"<<LCA(root,n1,n2)<<endl;
+    cout<<" LCA2 :"<<LCA2(root,n1,n2)->data<<endl;
 
 }
