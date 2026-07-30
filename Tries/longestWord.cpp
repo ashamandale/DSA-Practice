2c8faeb13caa82ee72e25ebc68ee6287cb6ef8c4
Asha Mandale <87373443+ashamandale@users.noreply.github.com>
2026-07-30 22:20:47 +0530

    code updated

---
+++ Tries/longestWord.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<unordered_map>
+using namespace std;
+
+class Node{
+    public:
+    unordered_map<char,Node*> children;
+    bool endOfword;
+    Node(){
+        endOfword=false;
+    }
+};   
+
+class Trie{
+        Node* root;
+    public:
+      Trie(){
+        root = new Node();
+      }   
+   void insert(string key){
+    Node* temp=root;
+
+    for(int i=0;i<key.size();i++){
+        if(temp->children.count(key[i]) == 0){
+            temp->children[key[i]] = new Node(); //Insert new key
+        }
+        temp=temp->children[key[i]];
+    }
+
+    temp->endOfword =true;
+   }  
+
+
+   void longestHelper(Node* root,string ans, string temp){
+    for(pair<char,Node*> child: root->children){
+        
+        if(child.second->endOfword){
+            temp += child.first;
+
+        if((temp.size() == ans.size() && temp < ans) || (temp.size() > ans.size())){
+            ans=temp;
+        }
+        
+        longestHelper(child.second,ans,temp);
+        temp=temp.substr(0,temp.size()-1);
+        }
+        
+    }
+   }
+
+   string longestStringWithEOW(){
+     string ans="";
+
+      longestHelper(root,ans,"");
+      return ans;
+   }
+
+
+   
+};
+
+string longestString(vector<string> dict){
+    Trie trie;
+    for(int i=0;i<dict.size();i++){
+        trie.insert(dict[i]);
+    }
+    return trie.longestStringWithEOW();
+   }
+
+
+int main(){
+    vector<string> dict ={"a","banana","app","appl","ap","apply","apple"};
+    cout<<longestString(dict)<<endl;
+    return 0;
+
+}
