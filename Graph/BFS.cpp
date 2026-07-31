901007d62f751ed919c54107ef6fcfd7b610cdaf
Asha Mandale <87373443+ashamandale@users.noreply.github.com>
2026-07-31 20:23:46 +0530

    code updated

---
+++ Graph/BFS.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<vector>
+#include<list>
+#include<queue>
+using namespace std;
+
+class Graph{
+    int V;
+    list<int>* l;
+public:
+    Graph(int V){
+        this->V= V;
+        l=new list<int> [V];
+    }
+
+    void addEdge(int u, int v){    //u--v
+          l[u].push_back(v);
+          l[v].push_back(u);
+          
+    }
+
+
+    void print(){
+        for(int u=0; u<V ;u++){  // 0,1,2,3,4
+            list<int> neighbours = l[u];
+            cout<< u <<" : ";
+            for(int V : neighbours){
+               cout<<V<<" ";
+            }
+             cout<<endl;
+        }
+    }
+    void bfs(){
+        queue<int> q;
+        vector<bool> vis(V,false);
+        q.push(0);
+        vis[0] =true;
+
+        while(q.size() > 0){
+            int u = q.front(); //current vertex
+            q.pop();
+            cout<<u<<" ";
+
+            list<int> neighbours = l[u];
+            for(int v : neighbours){
+                if(!vis[v]){
+                    vis[v]=true;
+                    q.push(v);
+                }
+            }
+        }
+
+    }
+};
+
+int main(){
+     Graph graph(7);
+
+     graph.addEdge(0,1);
+     graph.addEdge(0,2);
+     graph.addEdge(1,3);
+     graph.addEdge(2,4);
+     graph.addEdge(3,4);
+     graph.addEdge(3,5);
+     graph.addEdge(4,5);
+     graph.addEdge(5,6);
+     
+     graph.bfs();
+     return 0;
+}
